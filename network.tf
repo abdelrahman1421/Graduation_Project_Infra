0dd0c53a6e30648f740ffd3dc912f7749cc86cf4
Abdelrahman Mohamed <abdohamoda636@gmail.com>
2022-11-14 14:19:17 +0200

    Adding Management & Restricted Subnets

--- network.tf
+++ network.tf
@@ -4,3 +4,22 @@ resource "google_compute_network" "project-vpc" {
   routing_mode            = "REGIONAL"
   mtu                     = 1460
 }
+
+resource "google_compute_subnetwork" "management-subnet" {
+  name          = "management-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.project-vpc.id
+  private_ip_google_access = true
+
+  
+}
+
+resource "google_compute_subnetwork" "restricted-subnet" {
+  name          = "restricted-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.project-vpc.id
+  private_ip_google_access = true
+  
+}
