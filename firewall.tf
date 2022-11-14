21e42509a007161191bbf9db745a7cbc73baa544
Abdelrahman Mohamed <abdohamoda636@gmail.com>
2022-11-14 14:52:03 +0200

    Add Firewall

---
+++ firewall.tf
@@ -0,0 +1,10 @@
+resource "google_compute_firewall" "allow-iap" {
+  name    = "allow-iap"
+  network = google_compute_network.project-vpc.id
+  allow {
+    protocol = "tcp"
+    ports    = ["22" , "80"]
+  }
+  direction     = "INGRESS"
+  source_ranges = ["35.235.240.0/20"]
+}
