2454e4843a569f89741efb8bef1316ed887215f9
Abdelrahman Mohamed <abdohamoda636@gmail.com>
2022-11-14 14:39:38 +0200

    Creating VM to connect to cluster

---
+++ vm.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "private-vm" {
+  name         = "private-vm"
+  machine_type = "e2-meduim"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.project-vpc.id
+    subnetwork = google_compute_subnetwork.management-subnet.id
+  }
+
+}
